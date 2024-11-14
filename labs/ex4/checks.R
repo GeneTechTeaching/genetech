fc5cf5b6c7ca85e76d10c14e60bed08794c15625
jemorlanes <jaem@kth.com>
2024-11-14 16:43:12 +0000

    removed solutions and updated checks

--- labs/ex4/checks.R
+++ labs/ex4/checks.R
@@ -20,11 +20,8 @@ q1_check <- function(ans) {
     # There are multiple hashes for ans 1, 2 and 3  as the type might be numeric or integer
     c("33b1e01702aeb4ac9461279b50600522", "e866442adfc27e602e85fc539e54b2a2"),
     c("184ef830db03c901113bf29dd948f40b", "b24267bdeff6c4733eeae69295db3b96"),
-    # c("219870d1e5b7a069479ad227f916a47f", "0275d8a1a76528d5a9c4372664cb73cb"), question 2
-    # c("4bc167cb7856f45421c6c2b1ffbfe3ad", "7124a9910cf6bcf65aa4ae484537d459"), question 3
     c("704d6af0611c6d93d5b0fe5898bcd121", "b24bd50a679ef9f43c714b29bd513600"), 
     c("90a9eb5f22816262c2a2f7d4321d1a8e")
-    # c("68414ede506de2617b66af8260ae88d1") question 4
   )
   checks <- c()
   for (i in seq_along(ans)) {
@@ -57,7 +54,7 @@ q4_check <- function(ans) {
   if (any(is.na(ans))) {
     stop("NA set as answer, make sure you filled it out properly.")
   }
-  hash <- "4950cee1e0aedad7d2033db32b82bdc8" #"22c55dc705445bb530072d2f01d9d769"
+  hash <- "4950cee1e0aedad7d2033db32b82bdc8"
   txt <- check_answer(sort(ans), hash)
 
   if (txt[[2]]) {
@@ -86,7 +83,7 @@ q7_check <- function(ans) {
   if (NA %in% ans) {
     stop("NA set as answer, make sure you filled it out properly.")
   }
-  hashes <- c("1b7a700f4285acc0bd40cfcb01a3a4f0", # "eaca5f783323cd3a0ebe04f752c86e3d", question 7.1
+  hashes <- c("1b7a700f4285acc0bd40cfcb01a3a4f0",
               "1b9595f8f162c53cb175888d3b9f2502")
   checks <- c()
   for (i in seq_along(ans)) {
@@ -107,7 +104,8 @@ q8_check <- function(ans) {
   if (NA %in% ans) {
     stop("NA set as answer, make sure you filled it out properly.")
   }
-  hashes <- c("4a5d7d50676e6d0ea065f445d8a5539d", "5e338704a8e069ebd8b38ca71991cf94")
+  hashes <- c("4a5d7d50676e6d0ea065f445d8a5539d", 
+              "5e338704a8e069ebd8b38ca71991cf94")
   checks <- c()
   for (i in seq_along(ans)) {
     txt <- check_answer(as.numeric(ans[[i]]), hashes[[i]])
@@ -129,7 +127,7 @@ q9_check <- function(ans) {
   }
 
   if (class(ans) != "character") cat(sprintf("Wrong format: '%s'", class(ans)), "\n")
-  hash <- "ad302d7ad8870a0462c8fb0b62f679d5"
+  hash <- "4a79118fc585788bbc9542e558ccd577"
   txt <- check_answer(sort(ans), hash)
   if (txt[[2]]) {
     cat(txt[[1]], "\n")
@@ -143,7 +141,8 @@ q10_check <- function(ans) {
   if (NA %in% ans) {
     stop("NA set as answer, make sure you filled it out properly.")
   }
-  hashes <- c("8341f760a5120c7266c6cbb62d43f5d6", "6791d6c73b1d07788f3a1b93b97cc6fd") # c("6dceff630cda09b9c89c61a8944b5223", "bd639ff1cd7fa3b0d5e8ccb9949bb90d")
+  hashes <- c("a60a133019a742f97ac5b4b7f27445f1", 
+              "60a157b68c0d3a76c2839528e2c12828")
   checks <- c()
   for (i in seq_along(ans)) {
     txt <- check_answer(ans[[i]], hashes[[i]])
@@ -164,7 +163,7 @@ q11_check <- function(ans) {
     stop("NA set as answer, make sure you filled it out properly.")
   }
 
-  if (class(ans) != "integer" || class(ans) != "numeric") {
+  if (class(ans) != "numeric") {
     stop(paste("Give the NUMBER for the answer you thinks is right. Not ", class(ans)))
   }
   ans <- as.numeric(ans)
@@ -186,7 +185,7 @@ q12_check <- function(ans) {
     stop(sprintf("Invalid class '%s'. The answer should be a 'data.frame'.", class(ans)))
   }
 
-  if (any(dim(ans) != c(40, 3))) {
+  if (any(dim(ans) != c(46, 3))) {
     stop(sprintf("Invalid dimensions %s, should be 40x3", paste(dim(ans), collapse = "x")))
   }
 
@@ -195,9 +194,9 @@ q12_check <- function(ans) {
   }
 
   hashes <- c(
-    gene = "18d421eecd417fb35826add66c802722", # "da1fbe2034e56944a7f74a6281894291",
-    weight = "fc42a6772129a0a660bccd83c61c4159", # "0c745a37d5f780dd6d0a2e97e58afaba",
-    factor = "271754dcc34104f023a1f83531c81beb"  #"c79c0f4fef754277307807afc6deb1f1"
+    gene = "51c98068a0e940071c3dd9e9f5395863", 
+    weight = "94e5977d22d9ad5d7f677bdefb29d9d8",
+    factor = "894093d92bae546c3397bf9e5f12e58f"
   )
   checks <- c()
   for (i in seq_along(ans)) {
@@ -229,7 +228,8 @@ q13_check <- function(ans) {
     stop("NA set as answer, make sure you filled it out properly.")
   }
 
-  hashes <- c("f2cc231f6fb7b06e4f4b1480dc4a5954", "4384228d77f8107753c2ff85195104f8")
+  hashes <- c("f2cc231f6fb7b06e4f4b1480dc4a5954", 
+              "4384228d77f8107753c2ff85195104f8")
   checks <- c()
   for (i in seq_along(ans)) {
     txt <- check_answer(ans[[i]], hashes[[i]])
